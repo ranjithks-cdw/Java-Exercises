8a640b49821a5dcbf1f9a1e67899bf53c2bb157d
ranjithks-cdw <ranjith.saravanan@cdw.com>
2023-03-13 21:27:57 +0530

    feat: Observations added for class inheritance

--- exercise1/ClassInheritance.java
+++ exercise1/ClassInheritance.java
@@ -27,3 +27,12 @@ class B {
 class C extends A {
 	public B bObj = new B();
 }
+
+/*
+	An object for the class C is created. As the class C is inheriting the class A, constructor of A is called 1st while creating C object. 
+	Then, its member variables definitions and declarations are assigned to the memory.
+	An object is created for B and its reference is a member variable of C, so constructor of B is called.
+
+	Finally, constructor of C(default) is called.
+	Constructors are used to initalize member variable at the time of object creation, so member variables are created first, then the constructor.
+*/
